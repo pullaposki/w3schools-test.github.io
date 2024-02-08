1b3be310c0de9f044d25b9dfbcced3de3c7b0a97
pullaposki <pullaposki@windowslive.com>
2024-02-08 18:18:32 +0200

    db update done - succeeded

---
+++ Migrations/20240208160536_init.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PriceCategories",
+                columns: table => new
+                {
+                    PriceCategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PriceCategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PriceCategories", x => x.PriceCategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    CompanyId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PriceCategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.CompanyId);
+                    table.ForeignKey(
+                        name: "FK_Companies_PriceCategories_PriceCategoryId",
+                        column: x => x.PriceCategoryId,
+                        principalTable: "PriceCategories",
+                        principalColumn: "PriceCategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShipRates",
+                columns: table => new
+                {
+                    ShipRateId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PerKg = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PerCubicMeter = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PerKm = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CompanyId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShipRates", x => x.ShipRateId);
+                    table.ForeignKey(
+                        name: "FK_ShipRates_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_PriceCategoryId",
+                table: "Companies",
+                column: "PriceCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShipRates_CompanyId",
+                table: "ShipRates",
+                column: "CompanyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShipRates");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropTable(
+                name: "PriceCategories");
+        }
+    }
+}
